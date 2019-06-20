f0671d909f00e0e685dfed88e8ed7e2016276b8f
gmeunier <gmeunier@sigma.fr>
2019-06-20 10:43:32 +0200

    wololo

--- web/src/app/index/index.component.ts
+++ web/src/app/index/index.component.ts
@@ -8,23 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class IndexComponent implements OnInit {
 
   constructor() { 
-    let nbr_serre = 3;
-    let obj_serre = [
+    const nbrSerre = 3;
+    const objSerre = [
       {
-        id_serre: 0,
+        id: 0,
         title: "fraise du bois de boulogne",
         description: "fraise au gout de miel frère",
-        plantation_date: new Date('December 17, 2019 03:24:00')
+        plantationDate: new Date('December 17, 2019 03:24:00')
       }, {
-        id_serre: 1,
+        id: 1,
         title: "vernifuge amérindien",
         description: "plante gustative proposant la capacité de pimenter vos soirée",
-        plantation_date: new Date('December 17, 2018 03:24:00')
+        plantationDate: new Date('December 17, 2018 03:24:00')
       }, {
-        id_serre: 2,
+        id: 2,
         title: "choux shiva",
         description: "espèce de choux descendant de la déesse shiva",
-        plantation_date: new Date('December 17, 2017 03:24:00')
+        plantationDate: new Date('December 17, 2017 03:24:00')
       }
     ];
   }
